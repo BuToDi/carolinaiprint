222a713f46457b6687c4e9f6005d8e80ce157554
Axel.B <axel.bouchayer@gmail.com>
2025-09-14 22:04:41 +0200

    oi

--- streamfiotte.py
+++ streamfiotte.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+from util import classify, set_background
 
 
 st.title('WildAIPrint')
 
+
+uploaded_file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+model = load_model('model.keras')
