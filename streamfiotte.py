1ff22c458615561ffc5e919fb54d012a91e1ff92
Axel.B <axel.bouchayer@gmail.com>
2025-09-14 23:06:32 +0200

    oui

--- streamfiotte.py
+++ streamfiotte.py
@@ -5,10 +5,28 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 
+df = pd.read_csv('dataphoto2.csv')
+
 
 st.title('WildAIPrint')
 
 
+model = load_model('model.keras')
+class_names = df['Espèce']
+
+
 uploaded_file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
-model = load_model('model.keras')
+
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    # classify image
+    class_name, conf_score = classify(image, model, class_names)
+
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
